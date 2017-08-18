26a6079432d01ff1c3aa474f08913a849b0c852a
renhong <renhong1@jd.com>
2017-08-18 15:02:30 +0800

    初始化工程

---
+++ myflow-service/src/main/java/com/hongshan/myflow/service/App.java
@@ -0,0 +1,13 @@
+package com.hongshan.myflow.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
